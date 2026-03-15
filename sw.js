8a27d111762cbe54dd09b6cc93bef469def09916
priamixdj <132686877+dev-priamix@users.noreply.github.com>
2026-03-15 13:22:27 +0100

    prova

--- sw.js
+++ sw.js
@@ -1,20 +1,57 @@
+// let timer = null;
+
+// self.addEventListener('message', (event) => {
+//     if (event.data.action === 'START_TIMER') {
+//         if (timer) clearInterval(timer);
+        
+//         timer = setInterval(() => {
+//             self.registration.showNotification("Notifica Attiva", {
+//                 body: "Loop da 30 secondi funzionante su GitHub!",
+//                 icon: "https://www.gstatic.com/images/branding/product/2x/googleg_96dp.png",
+//                 tag: "pwa-notif",
+//                 renotify: true,
+//                 vibrate: [100, 50, 100]
+//             });
+//         }, event.data.interval);
+//     }
+// });
+
+// self.addEventListener('install', () => self.skipWaiting());
+// self.addEventListener('activate', (event) => event.waitUntil(clients.claim()));
+
+
+
+// ---------------------------
+
 let timer = null;
+
+self.addEventListener('install', (event) => {
+    self.skipWaiting(); // Forza l'attivazione immediata
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(clients.claim());
+});
 
 self.addEventListener('message', (event) => {
     if (event.data.action === 'START_TIMER') {
         if (timer) clearInterval(timer);
         
+        // Prima notifica immediata per conferma
+        mostraNotifica("Sistema Partito!");
+
         timer = setInterval(() => {
-            self.registration.showNotification("Notifica Attiva", {
-                body: "Loop da 30 secondi funzionante su GitHub!",
-                icon: "https://www.gstatic.com/images/branding/product/2x/googleg_96dp.png",
-                tag: "pwa-notif",
-                renotify: true,
-                vibrate: [100, 50, 100]
-            });
-        }, event.data.interval);
+            mostraNotifica("Sono passati 30 secondi");
+        }, 30000);
     }
 });
 
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', (event) => event.waitUntil(clients.claim()));
+function mostraNotifica(testo) {
+    self.registration.showNotification("Notifica Android", {
+        body: testo,
+        icon: "https://www.gstatic.com/images/branding/product/2x/googleg_96dp.png",
+        vibrate: [200, 100, 200],
+        tag: "loop-30s",
+        renotify: true
+    });
+}
